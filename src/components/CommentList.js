4b6b9ef51fb9bdb73b16df1a73058101c0520f27
Mert Onur YILDIRIM <mert.yildirim@aartimuhendislik.com>
2021-09-23 16:51:47 +0300

    icon added

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Header, Image, List } from "semantic-ui-react";
+import { Header, List } from "semantic-ui-react";
 
 const CommentList = (props) => {
   return (
@@ -8,7 +8,7 @@ const CommentList = (props) => {
       <List relaxed>
         {props.comments.map((comment) => (
           <List.Item key={comment.id}>
-            <Image avatar src="public/logo192.png" />
+            <List.Icon name="github" size="large" verticalAlign="middle" />
             <List.Content>
               <List.Header as="span">{comment.display_name}</List.Header>
               <List.Description>{comment.body}</List.Description>
